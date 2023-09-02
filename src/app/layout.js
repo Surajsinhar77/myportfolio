ad903ddfdfc0aef97ce8c0ec8abc605783ccaa60
Suraj Kumar <surajsinhaking77@gmail.com>
2023-09-02 23:34:26 +0530

    font work done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={rubik.className}>{children}</body>
     </html>
   )
 }
