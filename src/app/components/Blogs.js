664a4bccfca2ff7dbfab0b0f7a57a7430aa035d0
Suraj Kumar <surajsinhaking77@gmail.com>
2023-09-04 03:17:38 +0530

    done this one

--- src/app/components/Blogs.js
+++ src/app/components/Blogs.js
@@ -8,18 +8,18 @@ function Blogs(){
                 <h2 className="text-2xl"> <Link href="#"> Converting a Laravel Inertia App from Mix to Vite </Link> </h2>
             </div>
             <div className="tags mt-5">
-                <Link href="#" className="px-7 py-2 text-red-500 rounded border-2 border-red-500">Next.js</Link>
+                <Link href="/" className="px-7 py-2 text-red-500 rounded border-2 border-red-500">Next.js</Link>
             </div>
             <div className="timeAndDate mt-4 text-gray-500">
                 <h3> <span>02 September 2023</span> / <span>4 minute read</span> </h3>
             </div>
             <div className="contentPreview mt-4">
                 <p>
-                    I recently converted my Inertia app from Laravel Mix to using Vite. In this post I'll talk about how it went, as well as some of the gotchas.
+                    I recently converted my Inertia app from Laravel Mix to using Vite. In this post I&apos;ll talk about how it went&rsquo; as well as some of the gotchas.
                 </p>
             </div>
             <div className="BlogBtn mt-5 text-blue-400 font-light">
-                <Link href="#" className=" flex items-center">
+                <Link href="/" className=" flex items-center">
                 Read Blogs  <span className="mt-0.5"> <FaArrowRight className="font-light"/> </span>
                 </Link>
             </div>
