0bbaf9ad3eda7a9110b32b24d3120dc9e321e48f
Suraj Kumar <surajsinhaking77@gmail.com>
2023-09-20 03:08:13 +0530

    ''

--- backend/controller/admins.controller.js
+++ backend/controller/admins.controller.js
@@ -1,12 +1,39 @@
 const adminModel = require('../model/admin.model');
+const bcrypt = require('bcrypt');
 
 const adminLogin = async(req, res) =>{
-	const 
+	// const 
 }
 
 const adminRegistration =async(req, res)=>{
+
+    async function hashPassword(myPlaintextPassword, saltRounds) {
+        return new Promise((resolve, reject) => {
+            bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(hash);
+                }
+            });
+        });
+    }
+
     const {name, email, password, role, phoneno, profilePic} = req.body;
 
+    
+    try{
+        const check = await adminModel.find({email:email});
+        if(check){
+            return res.status(403).json({msg: "User Already exist here ",userExist:true});
+        } 
+        const hash
+
+        })
+    }catch(err){
+        console.error(err);
+        return res.status(400).json({msg:"This is the Error ",err});
+    }
 }
 module.exports ={
     adminRegistration,
