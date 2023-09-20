c383413bd18afeff37ed2e7e56e3e6bf6221c393
suraj kumar <surajsinhakingr77>
2023-09-21 01:55:55 +0530

    done

--- backend/controller/admins.controller.js
+++ backend/controller/admins.controller.js
@@ -1,10 +1,6 @@
 const adminModel = require('../model/admin.model');
 const bcrypt = require('bcrypt');
 
-const adminLogin = async(req, res) =>{
-	// const 
-}
-
 const adminRegistration =async(req, res)=>{
 
     async function hashPassword(myPlaintextPassword, saltRounds) {
@@ -20,21 +16,46 @@ const adminRegistration =async(req, res)=>{
     }
 
     const {name, email, password, role, phoneno, profilePic} = req.body;
-
     
     try{
         const check = await adminModel.find({email:email});
         if(check){
             return res.status(403).json({msg: "User Already exist here ",userExist:true});
         } 
-        const hash
+        const hashPass = await hashPassword(password, 10);
 
+        const adminInfo = new adminModel({
+            name,
+            email,
+            hashPass,
+            role,
+            phoneno,
+            profilePic
         })
+
+        const result = await adminInfo.save();
+        return res.status(400).json({data : result, userExist:true});
     }catch(err){
         console.error(err);
         return res.status(400).json({msg:"This is the Error ",err});
     }
 }
+
+const adminLogin = async(req, res) =>{
+	// const 
+    const {email, password} = req.body;
+    try{
+        const check = await adminModel.findOne({email:email});
+        if(!check){
+            return res.status(409).json({msg:"User doesn't Exist", userExist:false});
+        }
+        
+    }catch(err){
+
+    }
+}
+
+
 module.exports ={
     adminRegistration,
     adminLogin,
